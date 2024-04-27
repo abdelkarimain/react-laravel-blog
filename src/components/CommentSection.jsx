f6adeadd26b963d33f2fd7b20664d0443438902f
abdelkarimain0 <abdelkarimain0@gmail.com>
2024-04-27 10:41:56 +0100

    add comment section to the post page

--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -1,14 +1,54 @@
-import { Button, Textarea } from 'flowbite-react';
-import React from 'react'
+import { Alert, Button, Textarea } from 'flowbite-react';
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 const CommentSection = ({ postId }) => {
+  const { auth_token } = useSelector((state) => state.user || 'null');
   const { currentUser } = useSelector((state) => state.user);
+  const [comment, setComment] = useState('');
+  const [commentError, setCommentError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (comment.length > 200 || comment.length === 0) {
+      return;
+    }
+
+    try {
+      const response = await fetch(`/api/comment/store`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${auth_token}`,
+        },
+        body: JSON.stringify({
+          post_id: postId,
+          content: comment,
+        }),
+      });
+
+      const responseData = await response.json();
+
+      if (response.ok) {
+        setComment('');
+        setCommentError(null);
+      }
+
+      if (!response.ok) {
+        // console.log(responseData);
+        setCommentError(responseData.error);
+      }
+
+    } catch (error) {
+      console.log(error.message);
+    }
+
+  };
 
   return (
 
-    <div className='max-w-4xl mx-auto w-full  text-lg p-3'>
+    <div className='max-w-4xl mx-auto w-full text-lg p-3'>
       {currentUser ? (
         <div className='flex items-center gap-1 my-5 text-gray-500'>
           <p>Signed in as:</p>
@@ -35,7 +75,7 @@ const CommentSection = ({ postId }) => {
 
       {currentUser && (
         <form
-          // onSubmit={handleSubmit}
+          onSubmit={handleSubmit}
           className='border border-teal-500 rounded-md p-3'
         >
           <Textarea
@@ -43,26 +83,22 @@ const CommentSection = ({ postId }) => {
             rows='2'
             maxLength='200'
             className='resize-none'
-          // onChange={(e) => setComment(e.target.value)}
-          // value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            value={comment}
           />
           <div className='flex justify-between items-center mt-5'>
-            <p className='text-gray-500 text-xs'>
-              {/* {200 - comment.length}  */}20
-              characters remaining
+            <p className='text-gray-500 text-sm'>
+              {200 - comment.length} characters remaining
             </p>
             <Button outline gradientDuoTone='purpleToBlue' type='submit'>
               Submit
             </Button>
           </div>
           {
-            // commentError
-            false
-            && (
-              <Alert color='failure' className='mt-5'>
-                {/* {commentError} */}
-                commentError
-              </Alert>
+            commentError === '' && (
+              <span className='mt-5 text-red-500 text-sm'>
+                {commentError}
+              </span>
             )}
         </form>
       )}
